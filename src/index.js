b7c04cb243ceb6ed5a572b0fbb35c94bb9321453
valpas312 <valenpalmas2005@gmail.com>
2025-09-11 16:22:51 -0300

    cors configurado para hacer pruebas con ngrok

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import pool from "./db.js";
+import cors from "cors";
 
 // Importar rutas
 import usersRouter from "./routes/users.js";
 import teammatesRouter from "./routes/teammates.js";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
